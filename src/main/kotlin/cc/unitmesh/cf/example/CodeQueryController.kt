8eb51553db8a145f4c19521f62c08aeed1e9dda7
Phodal Huang <h@phodal.com>
2023-09-19 20:02:12 +0800

    feat: make it works

--- src/main/kotlin/cc/unitmesh/cf/example/CodeQueryController.kt
+++ src/main/kotlin/cc/unitmesh/cf/example/CodeQueryController.kt
@@ -7,14 +7,13 @@ import cc.unitmesh.cf.example.semantic.CodeSemanticWorkflow
 import io.reactivex.rxjava3.core.Flowable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-@RestController("/api/code-query")
-class CodeQueryController(
-    val workflow: CodeSemanticWorkflow,
-) {
-
+@RestController
+@RequestMapping("/api/code-query")
+class CodeQueryController(val workflow: CodeSemanticWorkflow) {
     @PostMapping
     fun query(@RequestBody question: QuestionRequest): Flowable<WorkflowResult> {
         val stage = CodeSemanticWorkflow.ANALYSIS
